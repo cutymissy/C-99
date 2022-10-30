b45570b12f042657aa6567d0857181e3eb11cd86
cutymissy <91814388+cutymissy@users.noreply.github.com>
2022-10-29 18:45:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recgonition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recgonition.start();
+}
+
+recgonition.onresult = funciton(event) 
+ {
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    
+    document.getElementById("textbox").innerHTML = Content;
+    speak();
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speakdata = document.getElementById("textbox").value
+    var utterthis = new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utterthis);
+    Webcam.attach("camera");
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
